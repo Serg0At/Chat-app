38c7014e4132f89ce7308275b838e6a4068ba97a
Sergey <serg114454@gmail.com>
2025-09-13 17:48:05 +0400

    singup controller

---
+++ server/src/controllers/message.controller.js
@@ -0,0 +1,5 @@
+export default class MessageController {
+    static async sendMessage (req, res) {
+
+    }
+}
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import AuthController from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/signup', )
+router.post('/signup', AuthController.signup)
+router.post('/signin', AuthController.signin)
+router.post('/logout', AuthController.logout)
 
 export default router;
