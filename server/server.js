27d54b3cff208da0e26d3dd809275d2bbfc38702
Sergey <serg114454@gmail.com>
2025-09-13 12:17:21 +0400

    production ready

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
+import path from 'path';
 
 import authRoutes from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js'
+
+const PORT = process.env.PORT || 3000
 
 const app = express();
-const PORT = 5000
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
+
+// one of many ways for deployment, easy way because we deploy only backend and it serves both api and client side
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
