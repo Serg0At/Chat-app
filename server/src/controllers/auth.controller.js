2095a92747d458191782b1706e733be41e7504a4
Sergey <serg114454@gmail.com>
2025-09-14 16:05:16 +0400

    login-logout update

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -62,10 +62,37 @@ export default class AuthController {
     }
 
     static async signin (req, res) {
+        const { email, password } = req.body;
 
+        try {
+            const user = User.findOne({ email });
+
+            if (!user ) {
+                return res.status(400).json({ message: "Invalid credentials"})
+            }
+
+            const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+            if (!isPasswordCorrect) {
+                return res.status(400).json({ message: "Invalid credentials"})
+            }
+
+            generateToken(user._id, res);
+
+            res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic,
+            });
+        } catch (error) {
+            console.error("Error in login controller:", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
     }
 
     static async logout (req, res) {
-
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
     }
 }
