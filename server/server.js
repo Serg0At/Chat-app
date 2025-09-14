35a2116662b98e64d90f4adbb1f0ba6c006a5d2b
Sergey <serg114454@gmail.com>
2025-09-14 15:53:32 +0400

    welcome email implemented

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 
+import { ENV } from './src/lib/env.js';
 import { connectDB } from './src/lib/db.js';
 
 import authRoutes from './src/routes/auth.route.js';
 import messageRoutes from './src/routes/message.route.js'
 
-const PORT = process.env.PORT || 3000
+const PORT = ENV.PORT || 3000
 
 const app = express();
 const __dirname = path.resolve();
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 
 // one of many ways for deployment, easy way because we deploy only backend and it serves both api and client side
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")))
 
     app.get('*', (req, res) => {
