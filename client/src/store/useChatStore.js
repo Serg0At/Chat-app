c1b186c9eb65a6ead8293a56fa976c86ee62de59
Sergey <serg114454@gmail.com>
2025-09-19 17:55:43 +0400

    chat store toggle sound

--- client/src/store/useChatStore.js
+++ client/src/store/useChatStore.js
@@ -1,1 +1,18 @@
 import { create } from "zustand";
+
+export const useChatStore = create((set, get) => ({
+    allContacts: [],
+    chats: [],
+    messages: [],
+    activeTab: 'chats',
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    isSoundEnabled: localStorage.getItem('isSoundEnabled') === true,
+
+    toggleSound: () => {
+        localStorage.setItem('isSoundEnabled', !get().isSoundEnabled)
+        set({ isSoundEnabled: !get().isSoundEnabled })
+    },
+
+}))
